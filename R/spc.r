4dd77122df71491b7537e82861a3f720f0cbaf6d
Tom Jemmett <tom.jemmett@gmail.com>
2021-07-22 08:07:59 +0100

    resolves majority of lint warnings for spr.R

--- R/spc.r
+++ R/spc.r
@@ -14,13 +14,16 @@
 #' Field name can be specified using non-standard evaluation (i.e. no quotation marks).
 #' @param dateField Specify the field name which contains the date data, to be plotted on x axis.
 #' Field name can be specified using non-standard evaluation (i.e. no quotation marks).
-#' @param facetField Optional: Specify field name which contains a grouping/faceting variable. SPC logic will be applied to each group separately, with outputs combined. Currently accepts 1 variable only.
+#' @param facetField Optional: Specify field name which contains a grouping/faceting variable. SPC logic will be applied
+#'     to each group separately, with outputs combined. Currently accepts 1 variable only.
 #' Field name can be specified using non-standard evaluation (i.e. no quotation marks).
-#' @param options Optional: A list object containing additional control and formatting properties. Preferably created using the spcOptions function.
+#' @param options Optional: A list object containing additional control and formatting properties. Preferably created
+#'     using the spcOptions function.
 #'
 #' @export spc
 #'
-#' @return A ggplot2 object of the spc charts.  This will automatically print the plot, but can also be saved as an object if you want to manipulate it further.
+#' @return A ggplot2 object of the spc charts.  This will automatically print the plot, but can also be saved as an
+#'     object if you want to manipulate it further.
 #'
 #' @import dplyr
 #' @import ggplot2
@@ -56,11 +59,13 @@
 
 
 spc <- function(
-  data.frame
-  ,valueField
-  ,dateField
-  ,facetField = NULL
-  ,options = NULL ## options: target, trajectory, rebase, data as percentages, title, x title, y title, x axis break frequency, pointSize, returnChart, display legend
+  data.frame,
+  valueField,
+  dateField,
+  facetField = NULL,
+  ## options: target, trajectory, rebase, data as percentages, title, x title, y title, x axis break frequency,
+  #           pointSize, returnChart, display legend
+  options = NULL
 ) {
 
   # initialise variables
@@ -69,18 +74,18 @@ spc <- function(
   #validate all inputs.  Validation problems will generate an error and stop code execution.
   validateParameters(data.frame, valueField, dateField, facetField, options)
 
-  if(is.null(facetField)){ # If no facet field specified, bind a pseudo-facet field for grouping/joining purposes
+  if (is.null(facetField)) { # If no facet field specified, bind a pseudo-facet field for grouping/joining purposes
     facetField <- "pseudo_facet_col_name"
   }
 
   df <- spcStandard(data.frame, valueField, dateField, facetField, options)
 
   # Declare improvement direction as integer
-  if(!(is.null(options$improvementDirection))){
+  if (!(is.null(options$improvementDirection))) {
 
-    if(options$improvementDirection == "increase" || options$improvementDirection == 1){
+    if (options$improvementDirection == "increase" || options$improvementDirection == 1) {
       improvementDirection <- 1
-    } else if(options$improvementDirection == "decrease" || options$improvementDirection == -1){
+    } else if (options$improvementDirection == "decrease" || options$improvementDirection == -1) {
       improvementDirection <- -1
     }
   } else {
@@ -88,10 +93,10 @@ spc <- function(
   }
 
   #set output chart
-  if(!(is.null(options$outputChart))){ # Check if chart required as output
-    if(options$outputChart == TRUE){
+  if (!(is.null(options$outputChart))) { # Check if chart required as output
+    if (options$outputChart == TRUE) {
       outputChart <- 1
-    } else if(options$outputChart == FALSE){
+    } else if (options$outputChart == FALSE) {
       outputChart <- 0
     }
   } else {
@@ -99,87 +104,89 @@ spc <- function(
   }
 
   #set x axis breaks
-  if(!(is.null(options$xAxisBreaks))){
+  if (!(is.null(options$xAxisBreaks))) {
     xaxis <- df$x
-    start <- min(xaxis,na.rm = TRUE)
-    end <- max(xaxis,na.rm = TRUE)
+    start <- min(xaxis, na.rm = TRUE)
+    end <- max(xaxis, na.rm = TRUE)
     xaxislabels <- seq.Date(from = as.Date(start), to = as.Date(end), by = options$xAxisBreaks)
   } else {
     xaxislabels <- df$x
   }
 
   #set point size
-  if(!(is.null(options$pointSize))){
-    pointSize <- options$pointSize
+  pointSize <- if (is.null(options$pointSize)) {
+    2
   } else {
-    pointSize = 2
+    options$pointSize
   }
 
   #set x axis date format
-  if(!(is.null(options$xAxisDateFormat))){
+  if (!(is.null(options$xAxisDateFormat))) {
     xAxisDateFormat <- options$xAxisDateFormat
   } else {
     xAxisDateFormat <- "%d/%m/%Y"
   }
 
   #set main plot title
-  if(!(is.null(options$mainTitle))){
+  if (!(is.null(options$mainTitle))) {
     plottitle <- options$mainTitle
   } else {
     plottitle <- "SPC Chart"
   }
 
   #set x axis label
-  if(!(is.null(options$xAxisLabel))){
+  if (!(is.null(options$xAxisLabel))) {
     xlabel <- options$xAxisLabel
   } else {
     xlabel <- capitalise(dateField)
   }
 
   #set y axis label
-  if(!(is.null(options$yAxisLabel))){
+  if (!(is.null(options$yAxisLabel))) {
     ylabel <- options$yAxisLabel
   } else {
     ylabel <- capitalise(valueField)
   }
-  
+
   #set y axis breaks
-  if(!(is.null(options$yAxisBreaks))){
+  if (!(is.null(options$yAxisBreaks))) {
     yAxisBreaks <- options$yAxisBreaks
   } else {
     yAxisBreaks <- NULL
   }
 
   #set x axis fixed scaling for facet plots
-  if(!(is.null(options$fixedXAxisMultiple))){
+  if (!(is.null(options$fixedXAxisMultiple))) {
     scaleXFixed <- options$fixedXAxis
   } else {
     scaleXFixed <- TRUE
   }
 
   #set y axis fixed scaling for facet plots
-  if(!(is.null(options$fixedYAxisMultiple))){
+  if (!(is.null(options$fixedYAxisMultiple))) {
     scaleYFixed <- options$fixedYAxis
   } else {
     scaleYFixed <- TRUE
   }
-  facetScales <- if(scaleYFixed == TRUE && scaleXFixed == TRUE){ # For multiple facet chart, derived fixed/free scales value from x and y axis properties
+
+  # For multiple facet chart, derived fixed/free scales value from x and y axis properties
+  facetScales <- if (scaleYFixed == TRUE && scaleXFixed == TRUE) {
     "fixed"
-  } else if (scaleYFixed == TRUE && scaleXFixed == FALSE){
+  } else if (scaleYFixed == TRUE && scaleXFixed == FALSE) {
     "free_x"
-  } else if (scaleYFixed == FALSE && scaleXFixed == TRUE){
+  } else if (scaleYFixed == FALSE && scaleXFixed == TRUE) {
     "free_y"
-  } else if (scaleYFixed == FALSE && scaleXFixed == FALSE){
+  } else if (scaleYFixed == FALSE && scaleXFixed == FALSE) {
     "free"
   }
 
 
 
   #set percentage y axis
-  if(!(is.null(options$percentageYAxis))){ # Check if Y values are percentages
-    if(is.numeric(options$percentageYAxis)){
+  if (!(is.null(options$percentageYAxis))) { # Check if Y values are percentages
+    if (is.numeric(options$percentageYAxis)) {
       convertToPercentages <- options$percentageYAxis
-    } else if (is.logical(options$percentageYAxis)){
+    } else if (is.logical(options$percentageYAxis)) {
       convertToPercentages <- 0.1 * as.numeric(options$percentageYAxis)
     }
   } else {
@@ -192,7 +199,7 @@ spc <- function(
 
   ## Create ggplot using plot the dots colours OR output data frame ----
   # Create chart if required
-  if(outputChart == 1){
+  if (outputChart == 1) {
 
     #build a list of plotOptions to pass into the createGgplot() function
     plotOptions <- list(
@@ -211,11 +218,9 @@ spc <- function(
     plot <- createGgplot(df, facetField, plotOptions)
     return(plot)
 
-  } else if(outputChart == 0){
+  } else if (outputChart == 0) {
 
     #or return the calculated dataframe
     return(df)
   }
 }
-
-
