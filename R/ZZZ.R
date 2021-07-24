7675a0411f8d0e3a7b62eaa1297207de879b23ad
Tom Jemmett <tom.jemmett@gmail.com>
2021-07-24 06:21:43 +0100

    solves linting issue, gives a bit more explanation as to what this function is doing

--- R/ZZZ.R
+++ R/ZZZ.R
@@ -1,10 +1,22 @@
 #' null replacement
 #'
-#' if a is null, choose b
+#' if `a` is null, choose `b`.
 #'
+#' @param a a possibly null value
+#' @param b a value to replace `a` with if `a` is null
+#'
+#' @examples
+#'
+#' 1 %||% 2 == 1
+#' NULL %||% 2 == 2
+#'
+#' @name null-replacement
+
+# Exclude Linting
 `%||%` <- function(a, b) {
   if (is.null(a))
     b
   else
     a
 }
+# End Exclude Linting
