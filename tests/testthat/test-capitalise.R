fd7bf7d81fe631b2c75f1369f65e929932b53850
Tom Jemmett <tom.jemmett@gmail.com>
2021-07-25 16:30:56 +0100

    adds test for titleCase function

--- tests/testthat/test-capitalise.R
+++ tests/testthat/test-capitalise.R
@@ -1,5 +1,9 @@
-test_that("it correctly capitalises strings", {
+test_that("capitalize correctly capitalises strings", {
   expect_equal(capitalise("hello world"), "Hello world")
   expect_equal(capitalise("HELLO WORLD"), "HELLO WORLD")
   expect_equal(capitalise("1hi World"), "1hi World")
+})
+
+test_that("titleCase correctly converts a string to title case", {
+  expect_equal(titleCase("hello_world"), "Hello World")
 })
