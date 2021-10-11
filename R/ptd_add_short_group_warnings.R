c3d66b39d3246de9f0cee66d960cb9c82e993496
Tom Jemmett <tom.jemmett@gmail.com>
2021-10-11 09:58:20 +0100

    slight simplification of function

--- R/ptd_add_short_group_warnings.R
+++ R/ptd_add_short_group_warnings.R
@@ -3,10 +3,10 @@
 #' Adds a column to the spc data frame warning of any groups of points that are
 #' shorter than the warning_threshold, and which as a result do not follow best-
 #' practice for the number of points on which to base SPC calculations.
-#' Value is FALSE for any group (facet and rebase combined) that 
-#' is >= warning_threshold.  
-#' Value is TRUE for any group (facet and rebase combined) that 
-#' is < warning_threshold.  
+#' Value is FALSE for any group (facet and rebase combined) that
+#' is >= warning_threshold.
+#' Value is TRUE for any group (facet and rebase combined) that
+#' is < warning_threshold.
 #'
 #' @param .data A data frame containing the information to be plotted.
 #' @param warning_threshold An integer signifying the number of points that will
@@ -16,15 +16,8 @@
 #' @noRd
 #'
 ptd_add_short_group_warnings <- function(.data, warning_threshold = 12){
-
-  .data <- .data %>%
-    group_by(across(c("f", "rebase_group"))) %>%
-    mutate(short_group_warning = case_when(
-      n() < warning_threshold ~ TRUE,
-      TRUE ~ FALSE
-    )
-    ) %>%
-    relocate(.data$short_group_warning, .after = .data$rebase_group) %>%
+  .data %>%
+    group_by(across(c(.data$f, .data$rebase_group))) %>%
+    mutate(short_group_warning = n() < warning_threshold, .after = .data$rebase_group) %>%
     ungroup()
-
 }
