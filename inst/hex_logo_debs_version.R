9ac72d5f18f73ff0efdaf1614ff31d44ebe5cb23
Roberto Villegas-Diaz <bthillo@hotmail.com>
2022-11-08 10:12:05 +0000

    Update subplot: line & point size, include option to modify font, change NHS logo to just text

--- inst/hex_logo_debs_version.R
+++ inst/hex_logo_debs_version.R
@@ -7,11 +7,14 @@ fill_palette <-
     "#005EB8") # NHS blue
 fill_palette_breaks <-
   seq(.2, .8, length.out = length(fill_palette) - 1) * 6
-line_size <- 0.5
+line_size <- .75
 line_colour <- "#7F7F7F"
-point_size <- 2.9
+point_size <- 3.5
 point_shape <- 21
 point_stroke <- 0
+font_family <- "Arial"
+font_family_bold <- "Arial"
+font_family_bold_italic <- "Arial"
 y <- c(1, 3, 4, 6, 4, 3)
 demo_data_tb <- data.frame(
   x = 1:6,
@@ -37,25 +40,40 @@ subplot <- demo_data_tb %>%
                     size = 13,
                     fontface = "bold",
                     lineheight = .2,
+                    # family = font_family_bold,
                     hjust = 0) +
-  ggplot2::annotate(geom = "label",
+  ggplot2::annotate(geom = "text",
                     x = -0.5,
                     y = 2.2,
                     label = "NHS",
+                    colour = "#005EB8", # NHS Blue
                     size = 13,
-                    fontface = "bold.italic",
-                    label.padding = ggplot2::unit(.5, "mm"),
-                    label.r = ggplot2::unit(0, "lines"), # no round corners
-                    colour = "#FFFFFF",
-                    fill = "#005EB8",
+                    fontface = "bold",
+                    lineheight = .2,
+                    # family = font_family_bold,
                     hjust = 0) +
+  # ggplot2::annotate(geom = "label",
+  #                   x = -0.5,
+  #                   y = 2.2,
+  #                   label = "NHS",
+  #                   size = 16,
+  #                   fontface = "bold.italic",
+  #                   label.padding = ggplot2::unit(.5, "mm"),
+  #                   label.r = ggplot2::unit(0, "lines"), # no round corners
+  #                   colour = "#FFFFFF",
+  #                   fill = "#005EB8",
+  #                   # family = font_family,
+  #                   hjust = 0) +
   ggplot2::scale_fill_manual(values = fill_palette) +
   ggplot2::theme_void() + # remove all the features, except the main plot
   ggplot2::theme( # remove the legend
-    legend.position = "none"
+    legend.position = "none",
+    text = ggplot2::element_text(family = font_family)
   ) +
   hexSticker::theme_transparent() # make background of the plot transparent
 hex_logo(subplot,
          main_colour = line_colour,
+         url = "",
+         p_family = font_family,
          out_filename = "inst/images/logo_debs_version.png")
 
