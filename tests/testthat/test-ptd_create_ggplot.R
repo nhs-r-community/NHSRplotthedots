d5a4ad62997caf05dd21493b807959183e0eeb65
Fran Barton <francis.barton@nhs.net>
2024-08-06 12:26:57 +0100

    Try to avoid lintr error

--- tests/testthat/test-ptd_create_ggplot.R
+++ tests/testthat/test-ptd_create_ggplot.R
@@ -326,10 +326,7 @@ test_that("it sets the colour of the points based on the type", {
   d <- data.frame(x = as.Date("2020-01-01") + 1:20, y = rnorm(20)) |>
     # introduce some special cause variation!
     dplyr::mutate(
-      across("y", \(y) dplyr::case_when(
-        x > "2020-01-15" ~ y + 0.5,
-        TRUE ~ y
-      ))
+      across("y", \(y) dplyr::if_else(.data[["x"]] > "2020-01-15", y + 0.5, y))
     )
 
   colours_neutral <- list(
